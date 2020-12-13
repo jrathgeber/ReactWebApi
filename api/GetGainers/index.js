e85e89ba67b40413b5cbf98230e9460b08aade77
jrathgeber <jrathgeber@yahoo.com>
2020-12-13 08:51:08 -0500

    iex

--- api/GetGainers/index.js
+++ api/GetGainers/index.js
@@ -1,15 +1,46 @@
+console.log("Hello " + process.env.IEXCLOUD_PUBLIC_KEY );
+
+//process.env.IEXCLOUD_PUBLIC_KEY = "pk_2c691d5dd102497393020fadee099148"
+//process.env.IEXCLOUD_SECRET_KEY = "sk_652a78525ee442a3adb15c6e596151ee"
+//process.env.IEXCLOUD_API_VERSION = "stable"
+
+const iex = require('iexcloud_api_wrapper'); // gets auth from .env automatically
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+
+
+
+    const getMoverData = async() => {
+        try {
+          const gainers = await iex.list('gainers');
+          console.log(gainers);
+        }
+        catch(error) {
+          console.error(`Could not get data: ${error}`);
+          //process.exit(-1);  // nonzero exit code indicates failure
+        }
+      };
+
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    const hello = await getMoverData().gainers;
+
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: {
-            text: "Hello from the IEX"
-          }
+     // status: 200, /* Defaults to 200 */
+     //   body: {
+     //       text: "Hello from the IEX"
+     //     }
+
+     //     body:  { 
+     //       text : "Hi",
+     //       text2 : "Ho" + hello
+     //     }
+
+      body: await iex.list('gainers')
     };
 }
