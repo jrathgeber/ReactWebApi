a6bb9b8d19287d48e11d7b6b803b7fbc8b0dd3e3
Jason Rathgeber <jrathgeber@yahoo.com>
2021-12-03 21:17:43 -0500

    losers

---
+++ api/GetLosers/index.js
@@ -0,0 +1,41 @@
+const iex = require('iexcloud_api_wrapper'); // gets auth from .env automatically
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+
+
+    const getMoverData = async() => {
+        try {
+          const gainers = await iex.list('losers');
+          console.log(gainers);
+        }
+        catch(error) {
+          console.error(`Could not get data: ${error}`);
+          //process.exit(-1);  // nonzero exit code indicates failure
+        }
+      };
+
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    const hello = await getMoverData().gainers;
+
+    context.res = {
+      
+     // status: 200, /* Defaults to 200 */
+     //   body: {
+     //       text: "Hello from the IEX"
+     //     }
+
+     //     body:  { 
+     //       text : "Hi",
+     //       text2 : "Ho" + hello
+     //     }
+
+      body: await iex.list('losers')
+    };
+}
