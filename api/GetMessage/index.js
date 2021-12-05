b9e868b7b83ffab8d7ac8015b285121f7d2cec3a
Jason Rathgeber <jrathgeber@yahoo.com>
2021-12-04 20:39:45 -0500

    v3

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -9,7 +9,7 @@ module.exports = async function (context, req) {
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: {
-            text: "Hello from the API V2"
+            text: "Hello from the API V3"
           }
     };
 }
